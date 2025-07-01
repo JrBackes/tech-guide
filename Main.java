cf14799039630353e4333c617366933edfbc70e3
Junior Backes <junior.backes@hotmail.com.br>
2025-07-01 08:19:28 -0300

    ommit

--- Main.java
+++ Main.java
@@ -12,5 +12,13 @@ public class Main {
         Ticket ticket = new Ticket(1, "Problema de Login", "Não consigo acessar minha conta", junior, "Aberto");
 
         System.out.println(ticket.getCompId());
+
+        Department department2 = new Department("Atendimento", 2, "ATD");
+        
+        User junior2 = new User("Junior Backes", "junior.backes@hotmail.com", "password123", department2);
+    
+        Ticket ticket2 = new Ticket(2, "Problema de Login", "Não consigo acessar minha conta", junior2, "Aberto");
+
+        System.out.println(ticket2.getCompId());
     }
 }
